d0dcbf3fef42bc2985757dfc2b04a422fb03c57f
makstravm <maks7294@gmail.com>
2021-12-12 21:33:16 +0200

    added fix bag in App.tsx edit render page

--- src/components/Dialogs/DialogsMessage/DialogsMessage.tsx
+++ src/components/Dialogs/DialogsMessage/DialogsMessage.tsx
@@ -5,45 +5,45 @@ import style from '../Dialogs.module.css'
 
 
 type DialogsMessagePropsType = {
-  message: Array<MessageDataType>
-  newMessage: string
-  dispatch: (action: ActionsTypes) => void
+    message: Array<MessageDataType>
+    newMessage: string
+    dispatch: (action: ActionsTypes) => void
 }
 
 export const DialogsMessage = (props: DialogsMessagePropsType) => {
-  const message = props.message.map(m => {
-    return <div key={m.id} className={style.message}>
-      {m.message}
-    </div>
-  })
-  const onSendMessageClick = () => {
-    const correctSpaceNewPost = props.newMessage.trim()
-    if (correctSpaceNewPost) {
-      props.dispatch(SendMessageyAC(props.newMessage))
+    const message = props.message.map(m => {
+        return <div key={m.id} className={style.message}>
+            {m.message}
+        </div>
+    })
+    const onSendMessageClick = () => {
+        const correctSpaceNewPost = props.newMessage.trim()
+        if (correctSpaceNewPost) {
+            props.dispatch(SendMessageyAC(props.newMessage))
+        }
     }
-  }
-  const onNewMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    props.dispatch(UpdateNewMessageBodyAC(e.currentTarget.value))
-  }
-  const sendMessageKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === 'Enter') {
-      onSendMessageClick()
+    const onNewMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        props.dispatch(UpdateNewMessageBodyAC(e.currentTarget.value))
     }
-  }
-  return (<div>
-    < div className={style.messages}>
-      {message}
+    const sendMessageKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === 'Enter') {
+            onSendMessageClick()
+        }
+    }
+    return (<div>
+        < div className={style.messages}>
+            {message}
+        </div>
+        <div>
+            <textarea
+                placeholder='Enter your message'
+                value={props.newMessage}
+                onChange={onNewMessageChange}
+                onKeyPress={sendMessageKeyPress}
+            ></textarea>
+            <button
+                onClick={onSendMessageClick}>SEND</button>
+        </div>
     </div>
-    <div>
-      <textarea
-        placeholder='Enter your message'
-        value={props.newMessage}
-        onChange={onNewMessageChange}
-        onKeyPress={sendMessageKeyPress}
-      ></textarea>
-      <button
-        onClick={onSendMessageClick}>SEND</button>
-    </div>
-  </div>
-  )
+    )
 }
